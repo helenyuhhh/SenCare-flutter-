7aa29820e401e0617ad4e8db789964c9e5a3d641
Qianhui <helenyu301@gmail.com>
2025-03-24 12:34:59 -0400

    Finish search screen

--- lib/patientSearchDelegate.dart
+++ lib/patientSearchDelegate.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sencare/networkingManager.dart';
+import 'package:sencare/patientInfo.dart';
+
 // in progress
-class PatientSearchDelegate extends SearchDelegate{
+class PatientSearchDelegate extends SearchDelegate {
   var patientList = [];
   Future getAllPatientsFromAPI(String searchTerm) async {
     var list = await NetworkingManager().getAllPatientByName(searchTerm);
@@ -11,57 +13,79 @@ class PatientSearchDelegate extends SearchDelegate{
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return[
+    return [
       IconButton(
-        onPressed: () {
-          query = "";
-          patientList = [];
-        },
-        icon: const Icon(Icons.clear)
-      )
+          onPressed: () {
+            query = "";
+            patientList = [];
+          },
+          icon: const Icon(Icons.clear))
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    IconButton(
-      onPressed: (){
-        Navigator.pop(context);
-      },
-      icon: const Icon(Icons.arrow_back));
+    return IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.arrow_back));
   }
 
   @override
   Widget buildResults(BuildContext context) {
     return FutureBuilder(
-      future: getAllPatientsFromAPI(query), 
-      builder: (context, snapshot){
-        return ListView.builder(
-          itemCount: patientList.length,
-          itemBuilder:(context, index)=>ListTile(
-            title: Text(patientList[index]),
-            onTap: (){},
-
-          ));
-      });
+        future: getAllPatientsFromAPI(query),
+        builder: (context, snapshot) {
+          return ListView.builder(
+              itemCount: patientList.length,
+              itemBuilder: (context, index) => ListTile(
+                    title: Text(
+                        "${patientList[index]['name']['first']} ${patientList[index]['name']['last']}"),
+                    subtitle: Text(patientList[index]['room']),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PatientInfo(
+                                patientId: patientList[index]['_id'])),
+                      );
+                    },
+                  ));
+        });
   }
-  @override Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List filteredPatients = patientList.where((patient) {
+        String firstName = patient['name']['first'].toString().toLowerCase();
+        String lastName = patient['name']['last'].toString().toLowerCase();
+        String fullName = "$firstName $lastName";
+        String searchQuery = query.toLowerCase();
+        
+        return firstName.contains(searchQuery) || 
+               lastName.contains(searchQuery) || 
+               fullName.contains(searchQuery);
+      }).toList();
     return FutureBuilder(
-      future: getAllPatientsFromAPI(query),
-      builder: (context, snapshot){
-        return ListView.builder(
-          itemCount: patientList.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(patientList[index]),
-            onTap:(){
-              Navigator.pop(context);
-              Navigator.pushNamed(context, 'patientInfo',
-              arguments: [1, patientList[index] as String, 0.0, 0.0]);
-            }
-
-          ));
-      });
-
+        future: getAllPatientsFromAPI(query),
+        builder: (context, snapshot) {
+          return ListView.builder(
+              itemCount: filteredPatients.length,
+              itemBuilder: (context, index) => ListTile(
+                  title: Text(
+                      "${filteredPatients[index]['name']['first']} ${filteredPatients[index]['name']['last']}"),
+                  subtitle: Text(filteredPatients[index]['room']),
+                  onTap: () {
+                    //Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => PatientInfo(
+                              patientId: filteredPatients[index]['_id'])),
+                    );
+                    //arguments: [1, patientList[index] as String, 0.0, 0.0]);
+                  }));
+        });
   }
 }
--- lib/testsListScreen.dart
+++ lib/testsListScreen.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:sencare/testAddScreen.dart';
 import 'package:sencare/testInfoScreen.dart';
 import 'package:sencare/networkingManager.dart';
+import 'package:sencare/addPatient.dart';
 // this screen should be stateful
 
-class TextListScreen extends StatefulWidget{
+class TextListScreen extends StatefulWidget {
   final String patientId;
   const TextListScreen({Key? key, required this.patientId}) : super(key: key);
 
@@ -15,50 +16,65 @@ class TextListScreen extends StatefulWidget{
   }
 }
 
-class _TextListState extends State<TextListScreen>{
+class _TextListState extends State<TextListScreen> {
   final NetworkingManager _networkingManager = NetworkingManager();
   var testList = [];
   var personalTests = [];
   // fetch all the tests from the db
-  Future getAllTests() async{
+  Future getAllTests() async {
     var list = await _networkingManager.getAllTests(widget.patientId);
     testList = list;
     //personalTests = list.where((i) => i['patient_id']).toList();
-    personalTests = testList.where((test) => 
-        test['patient_id'] == widget.patientId // Match your API's key name
-      ).toList();
+    personalTests = testList
+        .where((test) =>
+                test['patient_id'] ==
+                widget.patientId // Match your API's key name
+            )
+        .toList();
     return list;
   }
   // var personalTests = testList
 
-
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Tests'),),
-      body: FutureBuilder(
-        future: getAllTests(), 
-        builder: (context, snapshot){
-          return ListView.builder(
-            itemCount: personalTests.length,
-            itemBuilder: (context, index)=>ListTile(
-              title: Text(personalTests[index]['category']),
-              subtitle: Text(personalTests[index]['date']),
-              onTap:(){
-                Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => 
-                            TestInfo(testId: personalTests[index]['id'], 
-                            patientId: widget.patientId)
-                            ),
-                        ).then((_){
-                        });
-              }
-
-            ));
-        })
-    );
+        appBar: AppBar(
+          title: const Text('Tests'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(child:
+            FutureBuilder(
+                future: getAllTests(),
+                builder: (context, snapshot) {
+                  return ListView.builder(
+                      itemCount: personalTests.length,
+                      itemBuilder: (context, index) => ListTile(
+                          title: Text(personalTests[index]['category']),
+                          subtitle: Text(personalTests[index]['date']),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => TestInfo(
+                                      testId: personalTests[index]['id'],
+                                      patientId: widget.patientId)),
+                            ).then((_) {});
+                          }));
+                }) ),
+            
+                ElevatedButton(
+                onPressed: () {
+                  // press to nagivate to addPatient
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => TestAdd(patientId: widget.patientId,)))
+                      .then((_){
+                      });
+                },
+                child: const Text("Add Test"))
+          ],
+        ));
   }
 }
