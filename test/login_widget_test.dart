c32ada9129b7cba58b105d92b2d4f7fc5b2247d2
Qianhui <helenyu301@gmail.com>
2025-04-13 01:37:47 -0400

    Test UI for login Screen

---
+++ test/login_widget_test.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sencare/loginScreen.dart';
+
+void main(){
+  group("test login widget", (){
+    testWidgets("Display login widget", (tester) async {
+    await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+    expect(find.text('SenCare'), findsOneWidget);
+    expect(find.byType(Image), findsOneWidget);
+// test how many widgets contains Username and Passwprd
+    expect(find.text('Username'), findsNWidgets(2));
+    expect(find.text('Password'), findsNWidgets(2));
+// test how many textinput
+    expect(find.byType(TextField), findsNWidgets(2));
+    expect(find.byType(ElevatedButton), findsOneWidget);
+  });
+
+  });
+  
+}
